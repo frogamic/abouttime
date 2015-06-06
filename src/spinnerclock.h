0633fe7568b5f02fb50ec646a6441e9e24f03ce0
Dominic Shelton <drdshelton@gmail.com>
2015-06-06 15:42:23 +1200

    Added doxygen style documenting global functions of spinnerclock

--- src/spinnerclock.h
+++ src/spinnerclock.h
@@ -8,9 +8,18 @@
 
 #define ANIMATION_DURATION 2000
 
+/** Increments the spinners, should be called every 2.5 minutes.
+ *  \param  data    dummy pointer to NULL to maintain compatibility with pebble API.
+ *  \brief Must be called after a call to spinners_create().
+ */
 void spinners_tick(void* data);
 
+/** Creates the text layers, initialises all spinner values, and sets readout to current time.
+ *  \param  layer   A pointer to the parent layer on which to draw the spinners.
+ */
 void spinners_create(Layer* layer);
 
+/** Destroys all resources associated with the spinners.
+ */
 void spinners_destroy();
 
