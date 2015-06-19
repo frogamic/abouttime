13df80cfaae103239e5457b3d650e4768b2d5b51
Dominic Shelton <drdshelton@gmail.com>
2015-06-20 01:32:18 +1200

    Added handler for settings window close event

--- config/config.js
+++ config/config.js
@@ -1,6 +1,14 @@
 // Open the settings dialog
 Pebble.addEventListener('showConfiguration',
     function () {
+        console.log('Showing config screen');
         Pebble.openURL('data:text/html,' + encodeURIComponent(config_html + '<!--.html'));
+    }
+);
+
+Pebble.addEventListener('webviewclosed',
+    function(e) {
+        var config = JSON.parse(decodeURIComponent(e.response));
+        console.log('Received config: ' + JSON.stringify(config));
     }
 );
