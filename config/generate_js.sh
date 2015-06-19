9b945ee1ad5a2bc135806a364a96e89bed5fd354
Dominic Shelton <drdshelton@gmail.com>
2015-06-19 21:54:45 +1200

    Changed directories to be relative from base dir

--- config/generate_js.sh
+++ config/generate_js.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 HTML_VAR="config_html"
-SOURCE_HTML="config.html"
-SOURCE_JS="config.js"
-DESTINATION_JS="../src/js/pebble-js-app.js"
+SOURCE_HTML="config/config.html"
+SOURCE_JS="config/config.js"
+DESTINATION_DIR="src/js/"
+DESTINATION_JS="pebble-js-app.js"
 COMMENT="// File generated automatically from files in /config. Do not change this file as it will be replaced"
 
-echo $COMMENT > $DESTINATION_JS
-echo var $HTML_VAR = \"$(sed -e 's/"/\\"/g' -e 's/^[ \t]*//' $SOURCE_HTML | tr -d '\n')\"\; $'\n' >> $DESTINATION_JS
-cat $SOURCE_JS >> $DESTINATION_JS
+# Create js directory in src.
+mkdir -p $DESTINATION_DIR
+# Write the comment to the file, overwriting its contents.
+echo $COMMENT > $DESTINATION_DIR$DESTINATION_JS
+# Append the escaped HTML to the file.
+echo var $HTML_VAR = \"$(sed -e 's/\\/\\\\/g' -e 's/"/\\"/g' -e 's/^[ \t]*//' -e 's/$/\\n/' $SOURCE_HTML | tr -d "\n")\"\; >> $DESTINATION_DIR$DESTINATION_JS
+# Append the rest of the JS to the file.
+cat $SOURCE_JS >> $DESTINATION_DIR$DESTINATION_JS
 
